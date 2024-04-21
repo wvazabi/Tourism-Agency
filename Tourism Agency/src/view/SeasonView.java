08041a425deea9f5ae40357c8b3df05a955bd6cd
Enes Kaya <eng.eneskaya@gmail.com>
2024-04-21 14:13:23 +0300

    Add All view

---
+++ Tourism Agency/src/view/SeasonView.java
@@ -0,0 +1,9 @@
+package view;
+
+import javax.swing.*;
+
+public class SeasonView {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton saveButton;
+}
---
+++ Tourism Agency/src/view/UserView.java
@@ -0,0 +1,10 @@
+package view;
+
+import javax.swing.*;
+
+public class UserView {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JComboBox comboBox1;
+    private JButton saveButton;
+}
