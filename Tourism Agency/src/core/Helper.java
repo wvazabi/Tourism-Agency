79c517681090224e49443aec280d47f105d28005
Enes Kaya <eng.eneskaya@gmail.com>
2024-04-21 14:52:04 +0300

    Login View field check

---
+++ Tourism Agency/src/core/Helper.java
@@ -0,0 +1,112 @@
+package core;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Helper {
+
+    public static void setTheme() {
+        // info döndürecek UI Manager set look and Feel i nimbus yapıyoruz
+        for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+            if ("Nimbus".equals(info.getName())) {
+                try {
+                    UIManager.setLookAndFeel(info.getClassName());
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+
+                break;
+            }
+        }
+    }
+
+    public static boolean isFieldListEmpty(JTextField[] fieldList){
+        for (JTextField field :fieldList){
+            if(isFieldEmpty(field)) return true;
+        }
+        return false;
+    }
+
+    //Static method textfielddların boş olup olmadığının kontrol eden boşsa true döndüren method
+    public static boolean isFieldEmpty(JTextField field) {
+        return field.getText().trim().isEmpty();
+    }
+
+    public static void showMsg(String str, String title) {
+        String msg;
+        String header = title;
+        switch (str) {
+            case "fill":
+                msg = "Please fill empty fields";
+                header = "Alert !";
+                break;
+            case "done":
+                msg = "Successfully completed";
+                header = "Completed";
+                break;
+            case "error":
+                msg = "Error";
+                header = "Error Message";
+            default:
+                msg = str;
+                header = title;
+        }
+
+        JOptionPane.showMessageDialog(null,
+                msg,
+                header,
+                JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public static void showMsg(String str) {
+        String msg;
+        String header;
+        switch (str) {
+            case "fill":
+                msg = "Please fill empty fields";
+                header = "Alert !";
+                break;
+            case "done":
+                msg = "Successfully completed";
+                header = "Completed";
+                break;
+            case "error":
+                msg = "Error";
+                header = "Error Message";
+            default:
+                msg = str;
+                header = "Message";
+        }
+
+        JOptionPane.showMessageDialog(null,
+                msg,
+                header,
+                JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public static int setLoc(String type, Dimension size) {
+
+        switch (type) {
+            case "x":
+                return (Toolkit.getDefaultToolkit().getScreenSize().width - size.width) / 2;
+            case "y":
+                return (Toolkit.getDefaultToolkit().getScreenSize().height - size.height) / 2;
+            default:
+                return 0;
+        }
+    }
+
+    public static boolean confirm(String str) {
+        String msg;
+        if (str.equals("sure")) {
+            msg = "Are you sure !!";
+
+        } else {
+            msg = str;
+        }
+        return JOptionPane.showConfirmDialog(null, msg, "Are you sure?", JOptionPane.YES_NO_OPTION) == 0;
+
+    }
+
+
+}
--- Tourism Agency/src/view/LoginView.java
+++ Tourism Agency/src/view/LoginView.java
@@ -1,20 +1,29 @@
 package view;
 
+import core.Helper;
+
 import javax.swing.*;
 
-public class LoginView extends JFrame{
+public class LoginView extends Layout{
     private JPanel container;
     private JPanel w_top;
     private JLabel lbl_title;
     private JPanel w_button;
-    private JTextField textField1;
-    private JButton loginButton;
-    private JPasswordField passwordField1;
+    private JTextField fld_username;
+    private JButton btn_login;
+    private JPasswordField fld_pass;
+    private JLabel lbl_username;
+    private JLabel lbl_pass;
 
     public LoginView() {
         this.add(container);
-        this.setTitle("Login View");
-        this.setSize(400,400);
-        this.setVisible(true);
+        this.guiInitilaze(400,400,"Tourism Agency");
+
+        btn_login.addActionListener(e -> {
+            if(Helper.isFieldEmpty(this.fld_username)) {
+                System.out.println("test");
+            }
+
+        });
     }
 }
